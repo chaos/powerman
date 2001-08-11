70a48cb29b4cfba4785a3543297adcfbaabae2a4
auselton <auselton@76eff002-5ed3-810f-47aa-be2f003abbef>
2001-08-11 00:20:34 +0000

    ClusterView update to new interface first efforts.  Also added some library and program bug fixes and updates.
    git-svn-id: https://powerman.googlecode.com/svn/trunk@24 76eff002-5ed3-810f-47aa-be2f003abbef

--- lib/powerlib.tcl
+++ lib/powerlib.tcl
@@ -5,6 +5,8 @@
 # See ../DISCLAIMER
 # v. 0-0-1:  2001-07-11
 # v. 0-0-2:  2001-07-31
+# v. 0-0-3:  2001-08-08
+#            allow "cluster" to be empty or reinitialized 
 ####################################################################
 #
 ####################################################################
@@ -21,111 +23,173 @@
 #
 ####################################################################
 
-namespace eval powerlib {}
+namespace eval powerlib {
+    variable cluster
+    variable nodes
+    variable locations
+    variable check_types
+    variable control_types
+# These are arrays indexed by a node
+    variable location
+    variable control
+    variable check
+    variable gifs
 
-proc powerlib::check {node_list onoff} {
-#
-#   "nodes_list" is a list of cluster node names (i.e. just a list, no 
-# structure).  "onoff" is either "on" or "off".  "check" returns 
-# the elements in the list whose state (on or off) matches the
-# value of the "onoff" parameter.   
-#   In order to determine the state of a node one must call a 
-# hardware dependent function.  Each "type" of power monitoring
-# hardware will require a different interface.  Using the details
-# supplied from the $cluster list this function assembles the
-# needed command in the string variable $typed_check.  There may
-# be more than one hardware type and more than one node of each
-# hardware type.  The nodes are partitioned by type in the 
-# configuration file.  This function passes the list ($check_nodes) 
-# for each type to $typed_check.  Each $node_struct in the 
-# list gives a list of objects with the structure {node value ...}
-# giving the node name and its hardware dependent values.
-#
-    set return_val {}
-    foreach type $powerlib::check_types {
-        set check_nodes {}
-	foreach node $node_list {
-	    if {[lindex $powerlib::check($node) 0] == $type} {lappend check_nodes $node}
+    proc check {node_list onoff} {
+	#
+	#   "nodes_list" is a list of cluster node names (i.e. just a list, no 
+	# structure).  "onoff" is either "on" or "off".  "check" returns 
+	# the elements in the list whose state (on or off) matches the
+	# value of the "onoff" parameter.   
+	#   In order to determine the state of a node one must call a 
+	# hardware dependent function.  Each "type" of power monitoring
+	# hardware will require a different interface.  Using the details
+	# supplied from the $cluster list this function assembles the
+	# needed command in the string variable $typed_check.  There may
+	# be more than one hardware type and more than one node of each
+	# hardware type.  The nodes are partitioned by type in the 
+	# configuration file.  This function passes the list ($check_nodes) 
+	# for each type to $typed_check.  Each $node_struct in the 
+	# list gives a list of objects with the structure {node value ...}
+	# giving the node name and its hardware dependent values.
+	#
+	variable check_types
+# These are arrays indexed by a node
+	variable check
+
+	set return_val {}
+	foreach type $check_types {
+	    set check_nodes {}
+	    foreach node $node_list {
+		if {[lindex $check($node) 0] == $type} {lappend check_nodes $node}
+	    }
+	    if {[llength $check_nodes] > 0} {
+		set return_val [concat $return_val [[set type]::check $onoff $check_nodes]]
+	    }
 	}
-	if {[llength $check_nodes] > 0} {
-            set return_val [concat $return_val [[set type]::check $onoff $check_nodes]]
+	return $return_val
+    }
+
+    proc power {node_list onoffreset} {
+	#
+	#   "node_list" is a list of cluster node names (i.e. just a list, no 
+	# structure).  "onoffreset" is either "on", "off", or "reset".  
+	# "power" does not return a value.  It carries out the desired 
+	# action on the specified list, if possible.
+	#   In order to change the state of a node one must call a 
+	# hardware dependent function.  Each "type" of power control
+	# hardware will require a different interface.  Using the details
+	# supplied from the $cluster list this function assembles the
+	# needed command in the string variable $typed_power.  See the 
+	# comments for "check" for a discussion of the structure of the
+	# list $do_nodes passed to $typed_power.
+	# 
+	variable control_types
+# These are arrays indexed by a node
+	variable control
+
+	foreach type $control_types {
+	    set do_nodes {}
+	    foreach node $node_list {
+		if {[lindex $control($node) 0] == $type} {lappend do_nodes $node}
+	    }
+	    if {[llength $do_nodes] > 0} {
+		[set type]::power $onoffreset $do_nodes
+	    }
 	}
     }
-    return $return_val
-}
-
-proc powerlib::power {node_list onoffreset} {
-#
-#   "node_list" is a list of cluster node names (i.e. just a list, no 
-# structure).  "onoffreset" is either "on", "off", or "reset".  
-# "power" does not return a value.  It carries out the desired 
-# action on the specified list, if possible.
-#   In order to change the state of a node one must call a 
-# hardware dependent function.  Each "type" of power control
-# hardware will require a different interface.  Using the details
-# supplied from the $cluster list this function assembles the
-# needed command in the string variable $typed_power.  See the 
-# comments for "check" for a discussion of the structure of the
-# list $do_nodes passed to $typed_power.
-# 
-    foreach type $powerlib::control_types {
-	set do_nodes {}
-        foreach node $node_list {
-	    if {[lindex $powerlib::control($node) 0] == $type} {lappend do_nodes $node}
-        }
-	if {[llength $do_nodes] > 0} {
-	    [set type]::power $onoffreset $do_nodes
+    
+    ####################################################################
+    # An apparent glaring omission from the list support set of functions
+    ####################################################################
+    proc lunique list {
+	#
+	# remove duplicates from a list
+	#
+	set return_list {}
+	foreach element $list {
+	    if { [ lsearch -exact $return_list $element] < 0 } {
+		lappend return_list $element
+	    }
 	}
+	return $return_list
     }
-}
+    
+    
+    
+    ####################################################################
+    # Library initializations
+    # 
+    ####################################################################
+    proc init {file} {
+	variable cluster
+	variable nodes
+	variable locations
+	variable check_types
+	variable control_types
+# These are arrays indexed by a node
+	variable location
+	variable control
+	variable check
+	variable gifs
 
-####################################################################
-# An apparent glaring omission from the list support set of functions
-####################################################################
-proc lunique list {
-#
-# remove duplicates from a list
-#
-    set return_list {}
-    foreach element $list {
-	if { [ lsearch -exact $return_list $element] < 0 } {
-	    lappend return_list $element
+	source $file
+	foreach node_struct $cluster {
+	    set node [lindex $node_struct 0]
+	    lappend nodes $node
+	    set location($node) [lindex $node_struct 1]
+	    lappend locations $location($node)
+	    set control($node) [lindex $node_struct 2]
+	    lappend control_types [lindex $control($node) 0]
+	    set check($node) [lindex $node_struct 3]
+	    lappend check_types [lindex $check($node) 0]
+	    set gifs($node) [lindex $node_struct 4]
 	}
+	if {[llength $nodes] < 1} {usage "I don't see any nodes"}
+	
+	set locations [::powerlib::lunique $locations]
+	set control_types [::powerlib::lunique $control_types]
+	set check_types [::powerlib::lunique $check_types]
+
+	foreach type $check_types {
+	    source $::app::lib_dir/$type.tcl
+	}	
+	foreach type $control_types {
+	    source $::app::lib_dir/$type.tcl
+	}
+
     }
-    return $return_list
-}
+    
+    proc fini {} {
+	variable cluster
+	variable nodes
+	variable locations
+	variable check_types
+	variable control_types
+# These are arrays indexed by a node
+	variable location
+	variable control
+	variable check
+	variable gifs
 
-
-
-####################################################################
-# Library initializations
-# 
-####################################################################
-proc powerlib::Init {file} {
-    source $file
-    foreach node_struct $cluster {
-	set node [lindex $node_struct 0]
-	lappend ::powerlib::nodes $node
-	set ::powerlib::location($node) [lindex $node_struct 1]
-	lappend ::powerlib::locations $::powerlib::location($node)
-	set ::powerlib::control($node) [lindex $node_struct 2]
-	lappend ::powerlib::control_types [lindex $::powerlib::control($node) 0]
-	set ::powerlib::check($node) [lindex $node_struct 3]
-	lappend ::powerlib::check_types [lindex $::powerlib::check($node) 0]
-	set ::powerlib::gifs($node) [lindex $node_struct 4]
+	foreach type $check_types {
+	    namespace delete $type
+	}
+	foreach type $control_types {
+	    namespace delete $type
+	}
+	unset cluster
+	unset nodes
+	unset locations
+	unset check_types
+	unset control_types
+# These are arrays indexed by a node
+	unset location
+	unset control
+	unset check
+	unset gifs
     }
-    if {[llength $::powerlib::nodes] < 1} {usage "I don't see any nodes"}
-
-    set ::powerlib::locations [lunique $::powerlib::locations]
-    set ::powerlib::control_types [lunique $::powerlib::control_types]
-    set ::powerlib::check_types [lunique $::powerlib::check_types]
 }
+# end of "namespace eval powerlib"
 
-powerlib::Init $app::config_file
 
-foreach type $powerlib::check_types {
-    source $app::lib_dir/$type.tcl
-}
-foreach type $powerlib::control_types {
-    source $app::lib_dir/$type.tcl
-}
---
+++ lib/quack_check.tcl
@@ -0,0 +1,66 @@
+####################################################################
+# $Id$
+# by Andrew C. Uselton <uselton2@llnl.gov> 
+# Copyright (C) 2000 Regents of the University of California
+# See ../DISCLAIMER
+# v. 0-0-1:  2001-07-11
+# v. 0-0-2:  2001-07-31
+####################################################################
+####################################################################
+# power monitoring type:
+# quack_check
+####################################################################
+
+namespace eval quack_check {}
+
+proc quack_check::check {dir node_list} {
+    set return_val {}
+    set code ""
+    if { $dir == "on" } {
+        set code "1"
+    }
+    if { $dir == "off" } {
+        set code "0"
+    }
+    if { $code == "" } {
+        puts "quack_check::check does not understand $dir"
+    } else {    
+	set config_file_base_name [file tail $app::config_file]
+	set state_file_name [format "/tmp/%s.state" $config_file_base_name]
+    
+	if { ! [file exists $state_file_name] } {
+	    quack_check::make_state_file $state_file_name
+	}
+	set file_fid [open $state_file_name r]
+	while { ! [eof $file_fid]} {
+	    set line [gets $file_fid]
+	    set mark [string first " " $line]
+	    incr mark -1
+	    if {$mark > 0} {
+		set node [string range $line 0 $mark]
+		foreach n $node_list {
+		    if {$node == $n} {
+			if {[string first $dir $line] >= 0} {
+			    lappend return_val $node
+			}
+		    }
+		}
+	    }
+	}                    ;# while
+	close $file_fid
+    }
+    return $return_val
+}
+
+#########################
+#  quack_check support functions
+#########################
+
+proc quack_check::make_state_file name {
+    set state_file [open $name w]
+    foreach node $powerlib::nodes {
+	puts $state_file "$node off"
+    }
+    close $state_file
+}
+
