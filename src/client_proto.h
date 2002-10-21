0679aa7ba6698ca24033e7966c1ecec3a00a9783
garlick <garlick@76eff002-5ed3-810f-47aa-be2f003abbef>
2002-10-21 14:19:12 +0000

    add beacon on/off to help message
    git-svn-id: https://powerman.googlecode.com/svn/trunk@374 76eff002-5ed3-810f-47aa-be2f003abbef

--- src/client_proto.h
+++ src/client_proto.h
@@ -85,17 +85,25 @@
 	"102 unknown: %s" 				CP_EOL	/* hostlists */
 #define CP_RSP_QUIT	"103 Goodbye"			CP_EOL
 #define CP_RSP_HELP 	\
-	"304 nodes            - query node list" 	CP_EOL	\
-	"304 status [<nodes>] - query power status"	CP_EOL	\
-	"304 on <nodes>       - power on" 		CP_EOL	\
-	"304 off <nodes>      - power off"		CP_EOL	\
-	"304 cycle <nodes>    - power cycle"	CP_EOL	\
-	"304 reset <nodes>    - hardware reset (if available)" CP_EOL \
-	"304 soft [<nodes>]   - query soft power status (if available)" CP_EOL \
-	"304 temp [<nodes>]   - query temperature (if available)" CP_EOL \
-	"304 beacon [<nodes>] - query beacon status (if available)" CP_EOL \
-	"304 help             - display help"	CP_EOL	\
-	"104 quit             - logout"		CP_EOL
+	"304 nodes              - query node list" 	CP_EOL	\
+	"304 status [<nodes>]   - query power status"	CP_EOL	\
+	"304 on <nodes>         - power on" 		CP_EOL	\
+	"304 off <nodes>        - power off"		CP_EOL	\
+	"304 cycle <nodes>      - power cycle"		CP_EOL	\
+	"304 reset <nodes>      - hardware reset (if available)" \
+							CP_EOL \
+	"304 soft [<nodes>]     - query soft power status (if available)" \
+							CP_EOL \
+	"304 temp [<nodes>]     - query temperature (if available)" \
+							CP_EOL \
+	"304 beacon [<nodes>]   - query beacon status (if available)" \
+							CP_EOL \
+	"304 beacon_on <nodes>  - set beacon to ON (if available)" \
+							CP_EOL \
+	"304 beacon_off <nodes> - set beacon to OFF (if available)" \
+							CP_EOL \
+	"304 help               - display help"		CP_EOL	\
+	"104 quit               - logout"		CP_EOL
 #define CP_RSP_COMPLETE	"105 Command completed successfully" CP_EOL
 #define CP_RSP_RAW	"305 %s: %s"			CP_EOL
 #define CP_RSP_RAW_DONE "106 Query complete"		CP_EOL
