e666057312f5cf0ca91167b8d48c6dd5719324aa
garlick <garlick@76eff002-5ed3-810f-47aa-be2f003abbef>
2008-05-30 19:23:12 +0000

    add more tests
    git-svn-id: https://powerman.googlecode.com/svn/trunk@908 76eff002-5ed3-810f-47aa-be2f003abbef

--- test/mkconf.pl
+++ test/mkconf.pl
@@ -3,18 +3,15 @@
 # $Id$
 #
 $maxnodes = $ARGV[0];
+$devpath = $ARGV[1];
 $nodespervpc = 16;
 $devs = $maxnodes / $nodespervpc;
-$pwd = `pwd`; chomp $pwd;
 
-print "include \"../etc/vpc.dev\"\n";
-print "port 10102  # std port + 1\n";
-print "tcpwrappers no\n";
-
+print "include \"$devpath/vpc.dev\"\n";
 for ($i = 0; $i < $devs; $i++) {
 	$m = $i * $nodespervpc;
 	$n = $m + $nodespervpc - 1;
-	print "device \"test$i\" \"vpc\" \"$pwd/vpcd -f |&\"\n";
+	print "device \"test$i\" \"vpc\" \"./vpcd -f |&\"\n";
 	print "node \"t[$m-$n]\" \"test$i\"\n";
 }
 
